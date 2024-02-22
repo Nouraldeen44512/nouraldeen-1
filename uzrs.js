fe6c235887377bf4574865840eaa1497e920f4dc
yaf3iaden <42390257+noorromas@users.noreply.github.com>
2024-02-22 16:00:07 +0300

    Update uzrs.js
    جاهز

--- uzrs.js
+++ uzrs.js
@@ -123,22 +123,21 @@ var l_itvaiah=setInterval(function(){
     }
 }, 10000);
 
-
 $("style").last().append(`
-#users .uzr.raken1 img.co {
+#users .uzr.raken img.co {
   width: 0px !important;
     height: 0px !important;
   margin-right: 1px;
 }
 
-#users .uzr.raken1 span.uhash{
+#users .uzr.raken span.uhash{
     -webkit-text-fill-color: transparent;
 width: 0px !important;
     height: 0px !important;
   margin-right: 1px;}
 
 
-#users .uzr.raken1 img.u-ico {
+#users .uzr.raken img.u-ico {
 margin-left: 5px;
 
 
@@ -149,7 +148,7 @@ margin-left: 5px;
     }
 
 
-#users .uzr.raken1 img.ustat {
+#users .uzr.raken img.ustat {
       width: 55px !important;
   border-radius: 100px  !important;
 height: 55px !important;
@@ -163,7 +162,7 @@ box-shadow: 0 0 0px rgb(0, 0, 0), inset 0 0 0px rgb(0, 0, 0), 0 0 0 0px #000;
 
       
     }
-#users .uzr.raken1 .fitimg.u-pic {
+#users .uzr.raken .fitimg.u-pic {
   border: 1.5px solid #fff;
 margin-left: -54px ;
 height: 52px !important;
@@ -174,7 +173,7 @@ margin-bottom: 5px !important;
 margin-top: 2.9px !important;
 }
 
-#users .uzr.raken1 .u-msg {
+#users .uzr.raken .u-msg {
  
 margin-left: 8px ;
 margin-top: 2px !important;
@@ -191,7 +190,7 @@ margin-top: 2px !important;
 
 
 
-#users .uzr.raken1 غاع {
+#users .uzr.raken {
  box-shadow: inset 0 0 0 rgba(0,0,0,.08),0 0 3px #000;
   margin-bottom: 3px !important;
   margin-top: 2px !important;
@@ -207,11 +206,11 @@ background-size: 100%;
 
 
 
-#users .uzr.raken1 .d-flex.fl {
+#users .uzr.raken .d-flex.fl {
   padding-right: 0px !important;
 }
 
-#users .uzr.raken1 .u-topic {
+#users .uzr.raken .u-topic {
   margin-left: 5px;
    -webkit-background-clip: text;
   
@@ -223,7 +222,7 @@ background-size: cover;
 }
 
 
-#users .uzr.raken1 .itarr_raken1 {
+#users .uzr.raken .itarr_raken {
 
   width: 23px;
 
@@ -233,14 +232,16 @@ margin-left: 28px;
 }
 
 
+
+
 `);
 var l_itvaiah=setInterval(function(){
     if(myid!=null){
         clearInterval(l_itvaiah);
-        $("#users .uzr:contains('𝐑𝐀𝐊𝐀𝐍...')").addClass('raken1');
-        $("div#users .uzr.nour1 .My_vedio1").click(function(){
+        $("#users .uzr:contains('𝐑𝐀𝐊𝐀𝐍...')").addClass('raken');
+        $("div#users .uzr.raken .My_vedio1").click(function(){
         $("#movon1").show();
-        $('#movon1').addClass('raken1');
+        $('#movon1').addClass('raken');
         
         $("#ifr_mov1").show(function(){
         $("#upro").modal("hide");
