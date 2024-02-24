3d2a710b12cff368671b1413d1ce9ced1e4089ad
yaf3iaden <42390257+noorromas@users.noreply.github.com>
2024-02-24 20:07:44 +0300

    Update uzrs.js
    ص

--- uzrs.js
+++ uzrs.js
@@ -71,8 +71,9 @@ var l_itvsaad=setInterval(function(){
     if(myid!=null){
         clearInterval(l_itvsaad);
         $("#users .uzr:contains('نـَوَر')").addClass('saad1');
-        $("#users .uzr.saad1 .fitimg.u-pic").append('<img class="saad_star1" src="https://i.ibb.co/5G9Ttb7/10.gif">');
-		$("#users .uzr.saad1 span.u-topic").after('<img id="mystory1" class="My_vedio1" title="ستوري" style="float: right;width: 20px;margin-top: 0px;margin-right: 5px;cursor: pointer;" src="https://i.ibb.co/47VS4nY/image.png">');
+        $("#users .uzr.saad1 .fitimg.u-pic").append('<img class="saad_star1" src="https://up6.cc/2023/02/167585425188951.png">');
+	$("#users .uzr.stl2 .c-flex.fr").append('<img class="My_tsor" src="https://i.ibb.co/ThKHx1N/image.png">');	
+	    $("#users .uzr.saad1 span.u-topic").after('<img id="mystory1" class="My_vedio1" title="ستوري" style="float: right;width: 20px;margin-top: 0px;margin-right: 5px;cursor: pointer;" src="https://i.ibb.co/47VS4nY/image.png">');
         $("#users .uzr.saad1 span.uhash, #users .uzr.saad1 img.co").hide(); 
         $("#upro").before('<div id="movon1" onclick="$(this).hide().html(``);" class="moveon1" style="display:none;width:100%;height:100%;z-index:999999;position: fixed;left: 0px;top: 0px;background-color: rgba(0, 0, 0, 0.6);">');
         $("div#users .uzr.saad1 .My_vedio1").click(function(){
