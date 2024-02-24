8c7da199d4efbecee7e361d5c1f05892cdc267eb
yaf3iaden <42390257+noorromas@users.noreply.github.com>
2024-02-24 19:55:30 +0300

    Update uzrs.js
    ئ

--- uzrs.js
+++ uzrs.js
@@ -67,35 +67,21 @@ margin-left: -21px;
 
 
 `);
-var zizo=setInterval(function(){
+var l_itvsaad=setInterval(function(){
     if(myid!=null){
-        clearInterval(zizo);
-        $("#users .uzr:contains('نـَوَرث')").addClass('saad1');
-        $("#users .uzr.saad1 .c-flex.fr").append('<img class="My_star1" src="https://i.ibb.co/5G9Ttb7/10.gif">');
-	$("#users .uzr.saad1 .c-flex.fr").append('<img class="My_tsor" src="https://i.ibb.co/ThKHx1N/image.png">');
-	$("#users .uzr.saad1 .c-flex.fr").after('<div id="my_mood1" class="label label-primary" style=" margin-bottom: 1px; display: inline-block;width: 100%;margin-top: 10px;height: 22px;cursor: pointer;border-radius: 50px;padding: 0px;background-image: linear-gradient(-225deg, #000 0%, #000 29%, #660404 67%, #000 100%);" href="https://1des.net/">رَجُلٌ فَقَدَ "دِفْْءَ" إِمْرَأَةٍ فَـ "تَجَمَّدْ</div>');
-        $("#users .uzr span.u-topic:contains('نـَوَرث')").after('<img id="mystory1" class="My_vedio1" title="ستوري" src="https://i.ibb.co/47VS4nY/image.png">');
-    
+        clearInterval(l_itvsaad);
+        $("#users .uzr:contains('نـَوَر')").addClass('saad1');
+        $("#users .uzr.saad1 .fitimg.u-pic").append('<img class="saad_star1" src="https://i.ibb.co/5G9Ttb7/10.gif">');
+		$("#users .uzr.saad1 span.u-topic").after('<img id="mystory1" class="My_vedio1" title="ستوري" style="float: right;width: 20px;margin-top: 0px;margin-right: 5px;cursor: pointer;" src="https://i.ibb.co/47VS4nY/image.png">');
         $("#users .uzr.saad1 span.uhash, #users .uzr.saad1 img.co").hide(); 
-
-
-        $("#upro").before('<div id="movon1" onclick="$(this).hide().html(``);" class="moveon1" style="display:none;width:100%;height:100%;z-index:999999;position: fixed;left: 0px;top: 0px;background-color: rgba(0, 0, 0, 0.6);">'); 
-        
-        
-        
-        $("div#users .uzr:contains('نـَوَرث') .My_vedio1").click(function(){
+        $("#upro").before('<div id="movon1" onclick="$(this).hide().html(``);" class="moveon1" style="display:none;width:100%;height:100%;z-index:999999;position: fixed;left: 0px;top: 0px;background-color: rgba(0, 0, 0, 0.6);">');
+        $("div#users .uzr.saad1 .My_vedio1").click(function(){
         $("#movon1").show();
-        
-        $('#movon1').addClass('saad1');
+        $('#movon1').addClass('athb1');
         $("#movon1").before('<div class="modal-dialog fr break" style="height: 338px; max-width: 340px; min-width: 300px; min-height: 599px;overflow: visible;margin-top:40px;"></div>');
-        
-        
-        $("#movon1").append('<div style="max-width: 340px; min-width: 300px; color:white; width: 338px; float: right;" class="modal-header label-primary"><span class="pull-right clickable badge"><i class="fa fa-times"></i></span><label style="margin:1px; float: right;" > My vedio </label></div>');
-          
-		  
-        $("#movon1").append('<iframe id="ifr_mov1" style="max-width: 340px;min-width: 300px;right: 0; top: 35px; left: auto;" class="modal in" role="dialog" style="z-index: 2100; display: none; style="max-width:240px;" src="https://www.youtube.com/embed/5bK8Hd2KAbM" allowfullscreen="" width="338" height="490" frameborder="0"></iframe>');
-        
-       $("#ifr_mov1").show(function(){
+        $("#movon1").append('<div style="max-width: 340px; min-width: 300px; color:white; width: 338px; float: right;" class="modal-header label-primary"><span class="pull-right clickable badge"><i class="fa fa-times"></i></span><label style="margin:1px; float: right;" > My vedio </label></div>');  
+        $("#movon1").append('<iframe id="ifr_mov1" style="max-width: 340px;min-width: 300px;right: 0; top: 35px; left: auto;" class="modal in" role="dialog" style="z-index: 2100; display: none; style="max-width:240px;" src="https://www.youtube.com/embed/nfTocUD0puI" allowfullscreen="" width="338" height="490" frameborder="0"></iframe>');
+        $("#ifr_mov1").show(function(){
         $("#upro").modal("hide");
     } ); 
 });
