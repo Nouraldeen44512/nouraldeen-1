6b4bd90d5bdf05472c8efd158540ee2c80581686
yaf3iaden <42390257+noorromas@users.noreply.github.com>
2024-02-24 03:15:15 +0300

    Update uzrs.js
    ي

--- uzrs.js
+++ uzrs.js
@@ -61,7 +61,7 @@ box-shadow: 0 0 3px rgb(0 0 0), inset 0 0 4px rgb(0 0 0), 0 0 0 0px #000;
 
 #users .uzr.s3ed1 .u-msg {
 margin-top: 2px;
-   -webkit-text-fill-color: #000;
+   -webkit-text-fill-color: #207A2C;
 margin-left: 5px;
 
   font-size: 89% !important;
@@ -81,7 +81,7 @@ margin-left: 5px;
    
   
   border: 1px solid #fff;
- background-image: url(https://up6.cc/2024/01/170622859220941.png);
+ background-image: url(https://up6.cc/2024/02/170864387694731.jpg);
 background-size: 100%;
 
     
