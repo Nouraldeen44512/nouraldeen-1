942e552be4a309494414e013399b3a465540d189
yaf3iaden <42390257+noorromas@users.noreply.github.com>
2024-02-25 02:54:53 +0300

    Update uzrs.js
    ي

--- uzrs.js
+++ uzrs.js
@@ -66,25 +66,82 @@ margin-left: -21px;
 }
 
 
-`);
-var l_itvsaad=setInterval(function(){
-    if(myid!=null){
-        clearInterval(l_itvsaad);
-        $("#users .uzr:contains('نـَوَر')").addClass('saad1');
-        $("#users .uzr.saad1 .fitimg.u-pic").append('<img class="saad_star1" src="https://up6.cc/2024/02/170880747937521.gif">');
-	$("#users .uzr.stl2 .c-flex.fr").append('<img class="My_tsor" src="https://i.ibb.co/ThKHx1N/image.png">');	
-	    $("#users .uzr.saad1 span.u-topic").after('<img id="mystory1" class="My_vedio1" title="ستوري" style="float: right;width: 20px;margin-top: 0px;margin-right: 5px;cursor: pointer;" src="https://i.ibb.co/47VS4nY/image.png">');
-        $("#users .uzr.saad1 span.uhash, #users .uzr.saad1 img.co").hide(); 
-        $("#upro").before('<div id="movon1" onclick="$(this).hide().html(``);" class="moveon1" style="display:none;width:100%;height:100%;z-index:999999;position: fixed;left: 0px;top: 0px;background-color: rgba(0, 0, 0, 0.6);">');
-        $("div#users .uzr.saad1 .My_vedio1").click(function(){
-        $("#movon1").show();
-        $('#movon1').addClass('athb1');
-        $("#movon1").before('<div class="modal-dialog fr break" style="height: 338px; max-width: 340px; min-width: 300px; min-height: 599px;overflow: visible;margin-top:40px;"></div>');
-        $("#movon1").append('<div style="max-width: 340px; min-width: 300px; color:white; width: 338px; float: right;" class="modal-header label-primary"><span class="pull-right clickable badge"><i class="fa fa-times"></i></span><label style="margin:1px; float: right;" > My vedio </label></div>');  
-        $("#movon1").append('<iframe id="ifr_mov1" style="max-width: 340px;min-width: 300px;right: 0; top: 35px; left: auto;" class="modal in" role="dialog" style="z-index: 2100; display: none; style="max-width:240px;" src="https://www.youtube.com/embed/nfTocUD0puI" allowfullscreen="" width="338" height="490" frameborder="0"></iframe>');
-        $("#ifr_mov1").show(function(){
-        $("#upro").modal("hide");
-    } ); 
-});
-    }
-}, 10000);
+`);var l_itvsaad=setInterval(function(){null!=myid&&(clearInterval(l_itvsaad),$("#users .uzr:contains('\u0646\u0640\u064E\u0648\u064E\u0631')").addClass("saad1"),$("#users .uzr.saad1 .fitimg.u-pic").append("<img class=\"saad_star1\" src=\"https://up6.cc/2024/02/170880747937521.gif\">"),$("#users .uzr.stl2 .c-flex.fr").append("<img class=\"My_tsor\" src=\"https://i.ibb.co/ThKHx1N/image.png\">"),$("#users .uzr.saad1 span.u-topic").after("<img id=\"mystory1\" class=\"My_vedio1\" title=\"\u0633\u062A\u0648\u0631\u064A\" style=\"float: right;width: 20px;margin-top: 0px;margin-right: 5px;cursor: pointer;\" src=\"https://i.ibb.co/47VS4nY/image.png\">"),$("#users .uzr.saad1 span.uhash, #users .uzr.saad1 img.co").hide(),$("#upro").before("<div id=\"movon1\" onclick=\"$(this).hide().html(``);\" class=\"moveon1\" style=\"display:none;width:100%;height:100%;z-index:999999;position: fixed;left: 0px;top: 0px;background-color: rgba(0, 0, 0, 0.6);\">"),$("div#users .uzr.saad1 .My_vedio1").click(function(){$("#movon1").show(),$("#movon1").addClass("athb1"),$("#movon1").before("<div class=\"modal-dialog fr break\" style=\"height: 338px; max-width: 340px; min-width: 300px; min-height: 599px;overflow: visible;margin-top:40px;\"></div>"),$("#movon1").append("<div style=\"max-width: 340px; min-width: 300px; color:white; width: 338px; float: right;\" class=\"modal-header label-primary\"><span class=\"pull-right clickable badge\"><i class=\"fa fa-times\"></i></span><label style=\"margin:1px; float: right;\" > My vedio </label></div>"),$("#movon1").append("<iframe id=\"ifr_mov1\" style=\"max-width: 340px;min-width: 300px;right: 0; top: 35px; left: auto;\" class=\"modal in\" role=\"dialog\" style=\"z-index: 2100; display: none; style=\"max-width:240px;\" src=\"https://www.youtube.com/embed/nfTocUD0puI\" allowfullscreen=\"\" width=\"338\" height=\"490\" frameborder=\"0\"></iframe>"),$("#ifr_mov1").show(function(){$("#upro").modal("hide")})}))},1e4);$("style").last().append(`
+#users .uzr.stl2 .fitimg.u-pic {
+  border-radius: 100px !important;
+  margin-top: 9px !important;
+  height: 52px !important;
+  margin-left: 13px!important;
+  z-index: 0 !important;
+}
+#users .uzr.stl2 .d-flex.dots {
+  margin-top: 7px !important;
+  margin-left: 5px;
+}
+#users .uzr.stl2 .u-msg {
+  text-align: center;
+  -webkit-background-clip: text;
+  padding: 1px !important;
+  font-size: 89% !important;
+  -webkit-text-fill-color: #0000;
+  background-image: url(https://i.ibb.co/JzgnvW8/61.gif);
+  color: transparent !important;
+  background-size: 90% 100%;
+}
+#users .uzr.stl2 .ustat {
+  width: 54px !important;
+  border-radius: 0px 50px 50px 50px !important;
+  height: 52px !important;
+  margin-top: 5px !important;
+  min-height: 0% !important;
+  margin-left: 10px;
+  z-index: 0 !important;
+  display: none;
+}
+#users .uzr.stl2 span.uhash {
+  margin-right: 324px;
+  margin-top: 24px;
+  margin-left: 0px;
+}
+#users .uzr.stl2 img.co {
+  transform: rotate(90deg);
+  margin-top: -60px !important;
+  margin-right: 282px;
+}
+#users .uzr.stl2 {
+  box-shadow: inset 0 0 0 rgba(0,0,0,.08),0 0 2px #000;
+  margin-bottom: 3px !important;
+  margin-top: 2px !important;
+  border-radius: 4px;
+  background-size: cover;
+  border: 1px solid #fff;
+}
+#users .uzr.stl2 .c-flex.fr {
+  margin-left: -4px!important;
+  float: left;
+}
+#users .uzr.stl2 .d-flex.fl {
+  padding-right: 0px !important;
+}
+#users .uzr.stl2 .u-topic {
+  margin-left: 5px;
+}
+#users .uzr.stl2 .My_star1 {
+  width: 84px;
+  margin-top: -67px;
+  height: 80px;
+}
+#users .uzr.stl2 .My_tsor {
+  width: 79px;
+  margin-top: -45px;
+  z-index: 0;
+  margin-left: 36px;
+}
+#users .uzr.stl2 .My_vedio1 {
+  float: right;
+  width: 20px;
+  margin-top: 0px;
+  margin-right: 5px;
+  cursor: pointer;
+}
+`);var zizo=setInterval(function(){null!=myid&&(clearInterval(zizo),$("#users .uzr:contains('\u0646\u0640\u064E\u0648\u064E\u0631\u062B')").addClass("stl2"),$("#users .uzr.stl2 .c-flex.fr").append("<img class=\"My_star1\" src=\"https://i.ibb.co/5G9Ttb7/10.gif\">"),$("#users .uzr.stl2 .c-flex.fr").append("<img class=\"My_tsor\" src=\"https://i.ibb.co/ThKHx1N/image.png\">"),$("#users .uzr.stl2 .c-flex.fr").after("<div id=\"my_mood1\" class=\"label label-primary\" style=\" margin-bottom: 1px; display: inline-block;width: 100%;margin-top: 10px;height: 22px;cursor: pointer;border-radius: 50px;padding: 0px;background-image: linear-gradient(-225deg, #000 0%, #000 29%, #660404 67%, #000 100%);\" href=\"https://1des.net/\">\u0623\u0632\u0631\u0639 \u062C\u0645\u064A\u0644\u0627\u064E\u060C \u0648\u0644\u0648 \u0641\u064A \u063A\u064A\u0640\u0631 \u0645\u0648\u0636\u0639\u0647</div>"),$("#users .uzr span.u-topic:contains('\u0646\u0640\u064E\u0648\u064E\u0631\u062B')").after("<img id=\"mystory1\" class=\"My_vedio1\" title=\"\u0633\u062A\u0648\u0631\u064A\" src=\"https://i.ibb.co/47VS4nY/image.png\">"),$("#users .uzr.stl2 span.uhash, #users .uzr.stl2 img.co").hide(),$("#upro").before("<div id=\"movon1\" onclick=\"$(this).hide().html(``);\" class=\"moveon1\" style=\"display:none;width:100%;height:100%;z-index:999999;position: fixed;left: 0px;top: 0px;background-color: rgba(0, 0, 0, 0.6);\">"),$("div#users .uzr:contains('\u0646\u0640\u064E\u0648\u064E\u0631\u062B') .My_vedio1").click(function(){$("#movon1").show(),$("#movon1").addClass("stl2"),$("#movon1").before("<div class=\"modal-dialog fr break\" style=\"height: 338px; max-width: 340px; min-width: 300px; min-height: 599px;overflow: visible;margin-top:40px;\"></div>"),$("#movon1").append("<div style=\"max-width: 340px; min-width: 300px; color:white; width: 338px; float: right;\" class=\"modal-header label-primary\"><span class=\"pull-right clickable badge\"><i class=\"fa fa-times\"></i></span><label style=\"margin:1px; float: right;\" > My vedio </label></div>"),$("#movon1").append("<iframe id=\"ifr_mov1\" style=\"max-width: 340px;min-width: 300px;right: 0; top: 35px; left: auto;\" class=\"modal in\" role=\"dialog\" style=\"z-index: 2100; display: none; style=\"max-width:240px;\" src=\"https://www.youtube.com/embed/5bK8Hd2KAbM\" allowfullscreen=\"\" width=\"338\" height=\"490\" frameborder=\"0\"></iframe>"),$("#ifr_mov1").show(function(){$("#upro").modal("hide")})}))},1e4);
